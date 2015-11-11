0f5c276d92aed20e19455614d3b704e6ca064919
VagrantYu <ap5552477@yahoo.com.tw>
2015-11-11 04:39:12 -0500

    v0.1

---
+++ sha1.js
@@ -0,0 +1,13 @@
+function run() {
+  var msg = parseInt(document.getElementById("input").innerHTML);
+/*  var c = document.getElementById("content");
+  var n = document.createElement("p");   
+  n.appendChild(document.createTextNode(sha1(msg)));
+  c.appendChild(n);       */
+  document.getElementById('input1').value = document.getElementById('span1').innerHTML;
+}
+function sha1(msg){
+  return msg;
+}
+
+
